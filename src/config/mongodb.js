34db9764dc9866a381af200a6e8e0c3ce09c95d8
thairyo <sonthai1310.works@gmail.com>
2022-07-13 14:34:07 +0700

    design Database.

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,29 +1,23 @@
 import { MongoClient } from "mongodb";
 import { env } from "*/config/environment";
 
+let dbInstance = null;
+
 export const connectDB = async () => {
   const client = new MongoClient(env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
 
-  try {
-    // Connect the client to the server
-    await client.connect();
-    console.log("Connected successfully to server");
+  // Connect the client to the server
+  await client.connect();
 
-    // List databases
-    await listDatabases(client);
-
-  } finally {
-    // Ensures that the client will close when finish/error
-    await client.close();
-  }
+  // Assign clientDB to our dbInstance
+  dbInstance = client.db(env.DATABASE_NAME);
 };
 
-const listDatabases = async (client) => {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log(databasesList);
-  console.log("Your databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+// Get Database Instance
+export const getDB = () => {
+  if (!dbInstance) throw new Error("Must connect to Database first!");
+  return dbInstance;
 };
--- src/server.js
+++ src/server.js
@@ -2,14 +2,22 @@ import express from "express";
 import { connectDB } from "*/config/mongodb";
 import { env } from "*/config/environment";
 
-const app = express();
+connectDB()
+  .then(() => console.log("Connected successfully to database server!"))
+  .then(() => bootServer())
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
-connectDB().catch(console.log);
+const bootServer = () => {
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.end("<h1>Hello world</h1><hr/>");
-});
+  app.get("/", async (req, res) => {
+    res.end("<h1>Hello world</h1><hr/>");
+  });
 
-app.listen(env.PORT, env.HOST, () => {
-  console.log(`Hello running at host name:${env.HOST}:${env.PORT}/`);
-});
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello running at host name:${env.APP_HOST}:${env.APP_PORT}`);
+  });
+};
