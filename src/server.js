2fd5d345c05494c53bd047aef838da8fe39ca75f
thairyo <sonthai1310.works@gmail.com>
2022-07-29 10:54:04 +0700

    Deploy thairyo trello api to heroku.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { corsOptions } from "*/config/cors";
 import { connectDB } from "*/config/mongodb";
-import { env } from "*/config/environment";
 import { apiV1 } from "*/routes/v1";
 
 connectDB()
@@ -24,7 +23,12 @@ const bootServer = () => {
   // Use APIs v1
   app.use("/v1", apiV1);
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`Hello running at host name:${env.APP_HOST}:${env.APP_PORT}`);
+  // app.listen(env.APP_PORT, env.APP_HOST, () => {
+  //   console.log(`Hello running at host name:${env.APP_HOST}:${env.APP_PORT}`);
+  // });
+
+  // Support heroku deploy
+  app.listen(process.env.PORT, () => {
+    console.log(`Hello running at host name: ${process.env.PORT}`);
   });
 };
